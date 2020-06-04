311d6f6b3efa6f72415d2e4e2980f2dd1d8c539f
Nhan <sieunhan1397@gmail.com>
2020-06-04 15:39:11 +0700

    add basic route

---
+++ routes/subscribers.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+//Getting all
+router.get('/', (req, res) => {});
+//Getting One
+router.get('/:id', (req, res) => {});
+//Creating One
+router.post('/', (req, res) => {});
+//Update One
+router.patch('/:id', (req, res) => {});
+//Delete One
+router.delete('/:id', (req, res) => {});
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const subscribersRouter = require('./routes/subscribers');
+require('dotenv').config();
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
+app.use(express.json());
+app.use('/subscribers', subscribersRouter);
+app.listen(3000, () => {
+  console.log('Server Started');
+});
